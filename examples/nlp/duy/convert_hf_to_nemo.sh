eedb71f67367d4b06a35b16362c21d600c061fdc
PhungVanDuy <pvduy23@gmail.com>
2024-11-18 02:46:34 +0000

    initial rlhf nemo

---
+++ examples/nlp/duy/convert_hf_to_nemo.sh
@@ -0,0 +1,3 @@
+python /home/ubuntu/NeMo/scripts/checkpoint_converters/convert_llama_hf_to_nemo.py \
+    --input_name_or_path meta-llama/Llama-3.1-8B-Instruct \
+    --output_path ckpts/mcore_llama-3.1-8b.nemo
